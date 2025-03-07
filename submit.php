38142cce1af82d449d242d456a3346f431d3b5f5
Rahul Maurya <rahulmaurya6055@gmail.com>
2025-03-07 21:26:52 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -16,38 +16,38 @@ $high_school_rollnumber = $_POST['high_school_rollnumber'];
 $city = $_POST['city'];
 $phone_number = $_POST['phone_number'];
 $email = $_POST['email'];
-
+$gender = $_POST['gender'];
+$Roll_Number=rand(1000000000, 9999999999);
 
 if ($password !== $conform_password) {
     die("Passwords do not match.");
 }
 
-$sql = "SELECT * FROM users WHERE user_name = ? AND password = ? ";
+$sql = "SELECT * FROM users WHERE user_name = ? OR password = ? AND phone_number = ? ";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("ss", $user_name, $password);
+$stmt->bind_param("ssi", $user_name, $password, $phone_number);
 $stmt->execute();
 $result = $stmt->get_result();
-
 if ($result->num_rows > 0) {
     $users = $result->fetch_assoc();
-   echo"Username already saved database";
-    
+    echo "<script> alert('Username Already Saved Database')</script>";
+    echo "<script>window.location.href='registation.html'</script>";
 }
 else{
-   
-   $sql = "INSERT INTO users (user_name, password, conform_password, name , high_school_rollnumber, city, phone_number, email) 
-   VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+   $sql = "INSERT INTO users (user_name, password, conform_password, name , high_school_rollnumber, city, phone_number, email,Roll_Number,gender) 
+   VALUES (?, ?, ?, ?, ?, ?, ?, ?,?,?)";
 
-   
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("ssssisis", $user_name, $password, $conform_password, $name, $high_school_rollnumber, $city, $phone_number, $email);
+$stmt->bind_param("ssssisisis", $user_name, $password, $conform_password, $name, $high_school_rollnumber, $city, $phone_number, $email, $Roll_Number,$gender);
 
 
 if ($stmt->execute()) {
-   echo "Registered Successfully";
+   echo "<script> alert('Registration Successfully')</script>";
+   echo "<script> window.close();</script>";
+   echo "<script>window.location.href='index.html','_blank'</script>";
 
 } else {
-   echo "Error: " . $sql . "<br>" . $conn->error;
+   echo "<script>alert(' . $sql . $conn->error')</script>";
 }
 }
 
