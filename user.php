6e5a3f42054a35eaa5e2666116900d083acb3fbf
Rahul Maurya <rahulmaurya6055@gmail.com>
2025-03-07 21:25:28 +0530

    Update user.php

--- user.php
+++ user.php
@@ -1,14 +1,12 @@
 <?php
 session_start();
 $message = "";
-$filename = "computerfundamental.html";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $servername = "localhost";
-    $username = "root";  
-    $password = "";  
+    $username = "root";
+    $password = "";
     $dbname = "OnlineExaminationSystem";
-
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
@@ -16,19 +14,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     $user_name = $_POST['user_name'];
     $password = $_POST['password'];
-
-    $sql = "SELECT name FROM users WHERE user_name = ? AND password = ?";
+    $sql = "SELECT name, Roll_Number, password FROM users WHERE user_name = ?";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $user_name, $password);
+    $stmt->bind_param("s", $user_name);
     $stmt->execute();
     $result = $stmt->get_result();
-    
+
     if ($result->num_rows > 0) {
-        $users = $result->fetch_assoc();
-        $_SESSION['name'] = $users['name']; 
-        echo "<script>window.location.href='subjectchoose.html?name=" . urlencode($_SESSION['name']) . "';</script>";
+        $user = $result->fetch_assoc();
+        if ($user['password'] === $password) {
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['Roll_Number'] = $user['Roll_Number'];
+
+            echo "<script>window.open('subjectchoose.html?name=" . urlencode($_SESSION['name']) . "&Roll_Number=" . urlencode($_SESSION['Roll_Number']) . "', '_blank');</script>";
+            echo "<script> var win = window.open('', '_self'); win.close(); </script>";
+
+        } else {
+            echo "<script>alert('Wrong Password')</script>";
+            echo "<script>window.close();</script>";
+            echo "<script>window.location.href='index.html','_blank';</script>";
+        }
     } else {
-        echo "Invalid username or password";
+        echo "<script>alert('Invalid Username')</script>";
+        echo "<script>window.location.href='index.html','_blank';</script>";
     }
     $stmt->close();
     $conn->close();
